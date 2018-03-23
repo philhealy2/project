fe72b4c7b094e8db55b0b218989036e2acced40c
philhealy2 <philhealy94@hotmail.com>
2018-03-23 01:44:56 +0000

    continue work modifications, nav bar, basic layout

--- src/App.js
+++ src/App.js
@@ -2,20 +2,115 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+ class ContactForm extends React.Component {
+      render() {
+        return (
+          <div className="container-fluid">
+            <div className="row">
+               <div className="col-sm-1" >
+                <button type="button" className="btn btn-success">Add Contact</button>
+              </div>              
+              <div className="col-sm-3" >
+                <input type="text" className="form-control" defaultValue="Enter name"/>
+              </div>
+              <div className="col-sm-3" >
+                <input type="text" className="form-control" defaultValue="Enter address"/>
+              </div>
+              <div className="col-sm-2" >
+                 <input type="text" className="form-control" defaultValue="Enter telephone"/>
+              </div>                             
+             </div>
+          </div>
+          );
+
+      }
+    }
+
+    class Contact extends React.Component {
+      render() {
+        return (
+            <div className="col-sm-3" >
+              <div className="panel panel-primary">
+                
+                <div className="panel-body"> 
+                  <p>Name:  { this.props.contact.name }</p>
+                    <p>Ingredients:    { this.props.contact.ingredients }</p>
+                    <p>Method:    { this.props.contact.method }</p>              
+                </div>
+                <div className="panel-footer"> 
+                  <div className="btn-group btn-group-justified" role="group" aria-label="...">
+                      <div className="btn-group" role="group">
+                        <button type="button" className="btn btn-default">Edit</button>
+                      </div>
+                      <div className="btn-group" role="group">
+                        <button type="button" className="btn btn-danger">Delete</button>
+                      </div>
+                  </div>                     
+                </div>          
+              </div>
+            </div>
+            ) ;
+        }
+    }
+
+    class ContactList extends React.Component {
+      render() {
+          let contactRows = this.props.contacts.map(
+                (c) => <Contact key={c.ingredients} contact={c} />
+          );
+          return (
+            <div className="container-fluid contacts">
+              <div className="row">
+                 {contactRows}  
+              </div>
+              </div>
+            ) ;
+        }
+    }
+
+   
+
+ 
+    
+
+   class Header extends React.Component {
+      render(){
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <header>
+        <div class="header">
+        <h1>Cooking Corner</h1>
+        <img src="chefs.jpg" />
+       <nav id="bar">
+        <ul>
+         <li><a href= "App.js"> Home </a></li>
+            <li><a href= "./info/initial-entries.js"> Recipes </a></li>
+            <li><a href= "./elements/add.js"> Add New</a></li>
+            <li><a href= "shoplist.js"> Shopping List </a></li>
+        </ul>    
+      </nav>
       </div>
+
+        </header>
     );
   }
 }
+ class RecipeApp extends React.Component {
+      render() {
+          return (
+                <div className="jumbotron">
+                   <Header noContacts={this.props.contacts.length} />
+                  
+                   <ContactList contacts={this.props.contacts}  />
+                </div>
 
-export default App;
+    
+
+          );
+      }
+    }
+
+
+
+
+
+export default RecipeApp;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import RecipeApp from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+    let contacts = [
+        { name: 'Carbonara',
+    ingredients: 'Pasta, Cheese, Eggs, Bacon, Philidelphia Cheese',
+    method: 'Boil pasta, mix ingredients. Bake in oven'
+  },
+  {
+    name: 'Asian Stir Fry',
+    ingredients:'Noodles, Chicken, Vegetables of choice',
+    method: 'Fry the chicken in the wok. Add in the Vegetables. Once Noodles cooked as in and mix together'
+  },
+  {
+    name: 'Chocolate Cake',
+    ingredients:'200g Cooking Chocolate, Flour, Eggs, Sugar, Butter',
+    method:'Cream the butter and sugar. Melt the Chocolate, add in the eggs to creamed mixture. Bake in the oven for 45mins'
+  }
+];
+
+   
+
+
+ReactDOM.render(<RecipeApp contacts={contacts}/>, document.getElementById('root'));
 registerServiceWorker();
+
